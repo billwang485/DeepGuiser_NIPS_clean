5e75bada495a2a4ffcb4740129077f9752f6e89d
Chenyu Wang <canglang_1105@qq.com>
2022-09-01 10:13:27 +0000

    clean code v10

--- white_board.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('items.yaml') as f:
-    data = yaml.load(f, Loader=yaml.FullLoader)
-    print(data)
-    print(data['num'])
-
-with open('items1.yaml', 'w') as f:
-    yaml.dump(data, f)
